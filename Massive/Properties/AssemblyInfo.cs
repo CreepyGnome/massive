e8daa1878766dc5f9e4bd1d561a57d80b26f3904
CreepyGnome <rsfoley@gnomesoft.com>
2013-01-04 14:31:09 -0700

    Added Solution and Project Files
    Added Visual Studio 2012 Solution/Project set to use .Net 4 Client
    Profile.

---
+++ Massive/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Massive")]
+[assembly: AssemblyDescription("A small, happy, data access tool that will love you forever.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rob Conery (robconery@gmail.com)")]
+[assembly: AssemblyProduct("Massive")]
+[assembly: AssemblyCopyright("Copyright ©  2009-2013 Rob Conery (robconery@gmail.com)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb76fb08-25d2-4049-8774-ac6e9d9dcb87")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
